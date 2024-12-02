eae19e43f00ec0432e1855dd67e86a0f2f3e3074
Biblioteca <vld-biblioteca@inacap.cl>
2024-12-02 20:43:30 -0300

    Poco progreso en añadir imagenes extras

--- blog/views.py
+++ blog/views.py
@@ -4,42 +4,6 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from .models import Post
 from .forms import PostForm, EditPostForm
 
-def frontpage(request):
-    posts =Post.objects.all()
-
-    return render(request,'frontpage/frontpage.html',{'posts': posts})
-
-
-def post_detail(request, slug, origin):
-    post= Post.objects.get(slug=slug)
-
-    return render(request, 'blog/post_detail.html' , {'post' : post})   
-
-def post_list(request):
-    posts =Post.objects.all()
-    origin_dir = 'blog/post.html'
-    return render(request,'blog/post_list.html',{
-        'posts': posts,
-        'dir_origin': origin_dir})
-
-def search_post(request):
-    if request.method == "POST":
-        searched = request.POST.get('searched')
-        posts = Post.objects.filter(title__contains= searched)
-        return render(request,
-                      'blog/post_search.html',
-                      {'searched': searched,
-                       'posts' : posts
-                       })
-    else:
-        return render(request, 'blog/post_search.html',{})
-    
-#gallery code
-def gallery_list(request):
-    posts=Post.objects.all()
-    return render(request,'gallery/gallery_list.html',{"posts" : posts} )
-
-
 #Class based views
 
 class BaseView(ListView):
@@ -65,3 +29,39 @@ class DeletePostView(DeleteView):
     model= Post
     template_name = 'rework/delete_post.html'
     success_url = reverse_lazy('home')
+
+#def frontpage(request):
+#    posts =Post.objects.all()
+#
+#    return render(request,'frontpage/frontpage.html',{'posts': posts})
+
+# def post_detail(request, slug, origin):
+#     post= Post.objects.get(slug=slug)
+#
+#     return render(request, 'blog/post_detail.html' , {'post' : post})   
+
+# def post_list(request):
+#     posts =Post.objects.all()
+#     origin_dir = 'blog/post.html'
+#     return render(request,'blog/post_list.html',{
+#         'posts': posts,
+#         'dir_origin': origin_dir})
+
+# def search_post(request):
+#     if request.method == "POST":
+#         searched = request.POST.get('searched')
+#         posts = Post.objects.filter(title__contains= searched)
+#         return render(request,
+#                       'blog/post_search.html',
+#                       {'searched': searched,
+#                        'posts' : posts
+#                        })
+#     else:
+#         return render(request, 'blog/post_search.html',{})
+    
+# gallery code
+# def gallery_list(request):
+#     posts=Post.objects.all()
+#     return render(request,'gallery/gallery_list.html',{"posts" : posts} )
+
+
